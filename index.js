0ecd6457abb7719f8895aa76f7230f8ec3536c73
euLita <100318502+euLita@users.noreply.github.com>
2023-11-17 18:49:12 +0000

    this is an new update

--- index.js
+++ index.js
@@ -3,24 +3,18 @@ Operador logico - Or.
 => Paga MEIA:
 - Menores de 18.
 - Idosos.
-=> Condição 2 para pagar MEIA:
+=> Condição 2 =>  para pagar MEIA:
 - Adultos (meia apenas com carteirinha).
 */
-const readlineSync = require('readline-sync');
 
+const readlineSync = require('readline-sync');
 let age = readlineSync.question('Input your age: ');
 let isWallet = readlineSync.question('have a wallet? ');
-// let isAdulta;
 if (age >= 18 && age < 60 && isWallet === "n") {
     console.log('intire')
-  if (age < 18 || age >= 60 || (isAdulta && isWallet)) {
+  if(age < 18 || age >= 60 && isWallet === "y") {
     console.log('half')
   }
 }
 
-/*if(isAdulta && !temCarteirinha){
-  console.log('INTEIRA')
-} else {
-  console.log('MEIA')
-}
-*/
+/** This is a version update */
