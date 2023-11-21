6b656bd70032d1b4515facc79bf34ae501e36ca7
euLita <100318502+euLita@users.noreply.github.com>
2023-11-21 19:34:14 +0000

    this code is with problem

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 /*
 Operador logico - Or.
 => Paga MEIA:
-- Menores de 18.
-- Idosos.
-=> Condição 2 =>  para pagar MEIA:
-- Adultos (meia apenas com carteirinha).
+- menores de 18.
+- idosos.
+=> Condição 2
+- para pagar MEIA:
+- adultos (meia apenas com carteirinha).
 */
 
-const readlineSync = require('readline-sync');
-let age = readlineSync.question('Input your age: ');
-let isWallet = readlineSync.question('have a wallet? ');
-if (age >= 18 && age < 60 && isWallet === "n") {
-    console.log('intire')
-  if(age < 18 || age >= 60 && isWallet === "y") {
-    console.log('half')
-  }
+let age = prompt('Input your age: ');
+let isWallet = prompt('have a wallet? ');
+
+age = parseInt(age);
+
+if ((age >= 18 || age < 60) && isWallet === "n") {
+  console.log('intire')
+} else if ((age < 18 || age >= 60) && isWallet === 'y') {
+  console.log('half')
 }
 
 /** This is a version update */
